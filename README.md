d60311b53ea7eb37fde6503635c18f107da52876
Vince Allen <vince@spotify.com>
2014-08-21 13:20:23 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,122 @@
+![Build status](https://travis-ci.org/vinceallenvince/borderpalette.svg?branch=master)
+
+# borderpalette
+
+Randomly select border styles from a range of presets.
+
+##Install
+
+To include borderpalette as a component in your project, use the node module.
+
+```
+npm install borderpalette --save
+```
+
+You can also use the [standalone version](https://github.com/vinceallenvince/borderpalette/releases/latest) and reference the js file from your document.
+
+```
+<html>
+  <head>
+    <script src="scripts/borderpalette.min.js" type="text/javascript" charset="utf-8"></script>
+  </head>
+  ...
+```
+
+##Usage
+
+The module exports a BorderPalette class. In a nodejs project, you access it via:
+
+```
+var BorderPalette = require('borderpalette').BorderPalette;
+var palette = new BorderPalette();
+```
+
+Add styles to your palette via addBorder(). Pass the min/max amounts plus the border style. The add function adds a random number between your min and max. In the example below, you are twice as likely to get a 'dotted' style from the palette. Notice you can also chain your addBorder() calls.
+
+```
+pal.addBorder({
+  min: 2,
+  max: 10,
+  style: 'solid'
+}).addBorder({
+  min: 2,
+  max: 20,
+  style: 'dotted'
+});
+```
+
+To retrieve a style from the palette, use getBorder().
+
+```
+var style = pal.getBorder();
+```
+
+In the browser, the module exposes a BorderPalette namespace.
+
+```
+<html>
+  <head>
+    <script src="scripts/borderpalette.js" type="text/javascript" charset="utf-8"></script>
+  </head>
+  <body>
+    <script>
+      var pal = new BorderPalette.BorderPalette();
+      pal.addBorder({
+        min: 2,
+        max: 10,
+        style: 'solid'
+      }).addBorder({
+        min: 2,
+        max: 20,
+        style: 'dotted'
+      });
+      var box = document.createElement('div');
+      box.style.cssText = 'width: 100px; height: 100px; borderWidth: 4px; borderColor: red;';
+      box.style.borderStyle = pal.getBorder();
+      document.body.appendChild(box);
+    </script>
+  </body>
+</html>
+```
+
+To learn how to use the various BorderPalette functions, please review [the docs](http://vinceallenvince.github.io/borderpalette/doc/).
+
+##Building this project
+
+This project uses [Grunt](http://gruntjs.com). To build the project first install the node modules.
+
+```
+npm install
+```
+
+Next, run grunt.
+
+```
+grunt
+```
+
+To run the tests, run 'npm test'.
+
+```
+npm test
+```
+
+To check test coverage run 'grunt coverage'.
+
+```
+grunt coverage
+```
+
+A pre-commit hook is defined in /pre-commit that runs jshint. To use the hook, run the following:
+
+```
+ln -s ../../pre-commit .git/hooks/pre-commit
+```
+
+A post-commit hook is defined in /post-commit that runs the Plato complexity analysis tools. To use the hook, run the following:
+
+```
+ln -s ../../post-commit .git/hooks/post-commit
+```
+
+View the [code complexity](http://vinceallenvince.github.io/borderpalette/reports/) report.
---
+++ public/scripts/borderpalette.js
@@ -0,0 +1,245 @@
+!function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.BorderPalette=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
+/*jshint supernew:true */
+/** @namespace */
+var Utils = {
+  name: 'Utils'
+};
+
+/**
+ * Extends the properties and methods of a superClass onto a subClass.
+ *
+ * @function extend
+ * @memberof Utils
+ * @param {Object} subClass The subClass.
+ * @param {Object} superClass The superClass.
+ */
+Utils.extend = function(subClass, superClass) {
+  function F() {}
+  F.prototype = superClass.prototype;
+  subClass.prototype = new F;
+  subClass.prototype.constructor = subClass;
+  subClass._superClass = superClass.prototype;
+};
+
+/**
+ * Generates a psuedo-random number within a range.
+ *
+ * @function getRandomNumber
+ * @memberof Utils
+ * @param {number} low The low end of the range.
+ * @param {number} high The high end of the range.
+ * @param {boolean} [flt] Set to true to return a float.
+ * @returns {number} A number.
+ */
+Utils.getRandomNumber = function(low, high, flt) {
+  if (flt) {
+    return Math.random()*(high-(low-1)) + low;
+  }
+  return Math.floor(Math.random()*(high-(low-1))) + low;
+};
+
+/**
+ * Re-maps a number from one range to another.
+ *
+ * @function map
+ * @memberof Utils
+ * @param {number} value The value to be converted.
+ * @param {number} min1 Lower bound of the value's current range.
+ * @param {number} max1 Upper bound of the value's current range.
+ * @param {number} min2 Lower bound of the value's target range.
+ * @param {number} max2 Upper bound of the value's target range.
+ * @returns {number} A number.
+ */
+Utils.map = function(value, min1, max1, min2, max2) { // returns a new value relative to a new range
+  var unitratio = (value - min1) / (max1 - min1);
+  return (unitratio * (max2 - min2)) + min2;
+};
+
+/**
+ * Adds an event listener to a DOM element.
+ *
+ * @function _addEvent
+ * @memberof System
+ * @private
+ * @param {Object} target The element to receive the event listener.
+ * @param {string} eventType The event type.
+ * @param {function} The function to run when the event is triggered.
+ */
+Utils.addEvent = function(target, eventType, handler) {
+  if (target.addEventListener) { // W3C
+    target.addEventListener(eventType, handler, false);
+  } else if (target.attachEvent) { // IE
+    target.attachEvent('on' + eventType, handler);
+  }
+};
+
+/**
+ * Converts degrees to radians.
+ *
+ * @function degreesToRadians
+ * @memberof Utils
+ * @param {number} degrees The degrees value to be converted.
+ * @returns {number} A number in radians.
+ */
+Utils.degreesToRadians = function(degrees) {
+  if (typeof degrees !== 'undefined') {
+    return 2 * Math.PI * (degrees/360);
+  } else {
+    if (typeof console !== 'undefined') {
+      throw new Error('Error: Utils.degreesToRadians is missing degrees param.');
+    }
+  }
+};
+
+/**
+ * Converts radians to degrees.
+ *
+ * @function radiansToDegrees
+ * @memberof Utils
+ * @param {number} radians The radians value to be converted.
+ * @returns {number} A number in degrees.
+ */
+Utils.radiansToDegrees = function(radians) {
+  if (typeof radians !== 'undefined') {
+    return radians * (180/Math.PI);
+  } else {
+    if (typeof console !== 'undefined') {
+      throw new Error('Error: Utils.radiansToDegrees is missing radians param.');
+    }
+  }
+};
+
+/**
+ * Constrain a value within a range.
+ *
+ * @function constrain
+ * @memberof Utils
+ * @param {number} val The value to constrain.
+ * @param {number} low The lower bound of the range.
+ * @param {number} high The upper bound of the range.
+ * @returns {number} A number.
+ */
+Utils.constrain = function(val, low, high) {
+  if (val > high) {
+    return high;
+  } else if (val < low) {
+    return low;
+  }
+  return val;
+};
+
+/**
+ * Determines if one object is inside another.
+ *
+ * @function isInside
+ * @memberof Utils
+ * @param {Object} obj The object.
+ * @param {Object} container The containing object.
+ * @returns {boolean} Returns true if the object is inside the container.
+ */
+Utils.isInside = function(obj, container) {
+  if (!obj || !container) {
+    throw new Error('isInside() requires both an object and a container.');
+  }
+
+  obj.width = obj.width || 0;
+  obj.height = obj.height || 0;
+  container.width = container.width || 0;
+  container.height = container.height || 0;
+
+  if (obj.location.x + obj.width / 2 > container.location.x - container.width / 2 &&
+    obj.location.x - obj.width / 2 < container.location.x + container.width / 2 &&
+    obj.location.y + obj.height / 2 > container.location.y - container.height / 2 &&
+    obj.location.y - obj.height / 2 < container.location.y + container.height / 2) {
+    return true;
+  }
+  return false;
+};
+
+/**
+ * Capitalizes the first character in a string.
+ *
+ * @function capitalizeFirstLetter
+ * @memberof Utils
+ * @param {string} string The string to capitalize.
+ * @returns {string} The string with the first character capitalized.
+ */
+Utils.capitalizeFirstLetter = function(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+module.exports = Utils;
+},{}],2:[function(_dereq_,module,exports){
+Utils = _dereq_('drawing-utils-lib');
+console.log(Utils);
+/**
+ * Creates a new BorderPalette object.
+ *
+ * Use this class to create a palette of border styles.
+ *
+ * @param {string|number} [opt_id=] An optional id. If an id is not passed, a default id is created.
+ * @constructor
+ */
+function BorderPalette(opt_id) {
+
+  /**
+   * Holds a list of border styles.
+   * @private
+   */
+  this._borders = [];
+
+  this.id = opt_id || BorderPalette._idCount;
+  BorderPalette._idCount++; // increment id
+}
+
+/**
+ * Increments as each BorderPalette is created.
+ * @type number
+ * @default 0
+ * @private
+ */
+BorderPalette._idCount = 0;
+
+BorderPalette.prototype.name = 'BorderPalette';
+
+/**
+ * Adds a random number of the passed border style to the 'borders' array.
+ *
+ * @param {Object} options A set of required options
+ *    that includes:
+ *    options.min {number} The minimum number of styles to add.
+ *    options.max {number} The maximum number of styles to add.
+ *    options.style {string} The border style.
+ */
+BorderPalette.prototype.addBorder = function(options) {
+
+  if (!options.min || !options.max || !options.style) {
+    throw new Error('BorderPalette.addBorder requires min, max and style paramaters.');
+  }
+
+  for (var i = 0, ln = Utils.getRandomNumber(options.min, options.max); i < ln; i++) {
+    this._borders.push(options.style);
+  }
+
+  return this;
+};
+
+/**
+ * @returns A style randomly selected from the 'borders' property.
+ * @throws {Error} If the 'borders' property is empty.
+ */
+BorderPalette.prototype.getBorder = function() {
+
+  if (this._borders.length > 0) {
+    return this._borders[Utils.getRandomNumber(0, this._borders.length - 1)];
+  }
+
+  throw new Error('BorderPalette.getBorder: You must add borders via addBorder() before using getBorder().');
+};
+
+module.exports.BorderPalette = BorderPalette;
+
+
+},{"drawing-utils-lib":1}]},{},[2])
+(2)
+});
